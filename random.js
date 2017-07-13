9222dd0ea5622b3a3833ae00150d4f63a8f04228
Bobby Shunk <Bobby@MacBook-Pro-5.local>
2017-07-13 16:07:27 -0500

    work on linking images properly with the JS and work on the random number formula

--- random.js
+++ random.js
@@ -1,12 +1,27 @@
-console.log("greetings");
+// creates the variable body to hold the element body from the html
+let output = document.getElementById("arrows");
 
-let body = document.getElementById("body");
+// defines function randomNumber
+function randomNumber(){
 
-function displayPic(){
+	// sets variable randNum to euqual a random value between 0 and 10
+	var randNum = Mathfloor ( ( Math.random() * 10) + 1);
+
+	// condition checking to see if the random number is less than or equal to 5
+	if (randNum <= 5) {
+
+		// puts the arrow_left image into the DOM
+		output.innerHTML = `<div><img src="../arrow_left.png"></div`;
 	
-}
+	// otherwise if false
+	}else {
 
-body.addEventListener("click", getRandNum)
+		// puts the arrow_right image into the DOM
+		output.innerHTML = `<div><img src="../arrow_right.png"></div`;
+	}
+};
+
+output.addEventListener("click", randomNumber)
 
 
 
