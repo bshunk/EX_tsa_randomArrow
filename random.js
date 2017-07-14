8c31f12d8a5eb116b22d1cfb1e7104e342701614
Bobby Shunk <Bobby@MacBook-Pro-5.local>
2017-07-14 09:31:42 -0500

    get the random formula to work and respond to the page with the user's click

--- random.js
+++ random.js
@@ -1,36 +1,17 @@
-// creates the variable body to hold the element body from the html
-let output = document.getElementById("arrows");
+// adds event listener to the whole window to listen for a click and then run randomNumber function
+window.addEventListener("click", randomNumber)
+
+// sets pics to equal the array of both needed images
 let pics = ["images/arrow_left.png", "images/arrow_right.png"];
 
 // defines function randomNumber
-function randomNumber(){
+function randomNumber() {
 
-	// sets variable randNum to euqual a random value between 0 and 10
-	var randNum = Mathfloor((Math.random()));
+	console.log("randomNumber is working")
+	// sets variable randNum to euqual a random value between 
+	let randArrow = pics[Math.floor(Math.random() * pics.length)];
 
-	// condition checking to see if the random number is less than or equal to 5
-	if (randNum <= 5) {
-
-		// puts the arrow_left image into the DOM
-		output.innerHTML = `<img id="arrows" src=${arrow_left.png}>`;
-	
-	// otherwise if false
-	}else {
-
-		// puts the arrow_right image into the DOM
-		output.innerHTML = `<img id="arrows" src=${arrow_right.png}>`;
+	// sets the body in the html to equal the value of the random image that's been generated
+	body.innerHTML = `<img id="pic" src=${randArrow}>`
 };
-
-output.addEventListener("click", randomNumber)
-
-
-
-
-
-
-
-
-
-
-
 
