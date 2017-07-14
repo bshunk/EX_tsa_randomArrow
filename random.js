88d7116984bf4aad52520aa2cd10c6e9841bc83f
Bobby Shunk <Bobby@MacBook-Pro-5.local>
2017-07-14 09:22:34 -0500

    rework code to make the math.floor to work correctly

--- random.js
+++ random.js
@@ -1,24 +1,24 @@
 // creates the variable body to hold the element body from the html
 let output = document.getElementById("arrows");
+let pics = ["images/arrow_left.png", "images/arrow_right.png"];
 
 // defines function randomNumber
 function randomNumber(){
 
 	// sets variable randNum to euqual a random value between 0 and 10
-	var randNum = Mathfloor ( ( Math.random() * 10) + 1);
+	var randNum = Mathfloor((Math.random()));
 
 	// condition checking to see if the random number is less than or equal to 5
 	if (randNum <= 5) {
 
 		// puts the arrow_left image into the DOM
-		output.innerHTML = `<div><img src="../arrow_left.png"></div`;
+		output.innerHTML = `<img id="arrows" src=${arrow_left.png}>`;
 	
 	// otherwise if false
 	}else {
 
 		// puts the arrow_right image into the DOM
-		output.innerHTML = `<div><img src="../arrow_right.png"></div`;
-	}
+		output.innerHTML = `<img id="arrows" src=${arrow_right.png}>`;
 };
 
 output.addEventListener("click", randomNumber)
